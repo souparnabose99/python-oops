0307a1a2bd93329b12898de18c32976e089de24c
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2024-01-23 14:57:48 +0530

    Update strings.py

--- objects-and-data-structures/strings.py
+++ objects-and-data-structures/strings.py
@@ -1,14 +1,41 @@
 # Mod is used to get remainder -> (/)
 # eg 7/4 -> 3, but 7//4 -> 1.75
 
-# String Indexing
-
-
 # String Operations
 
+# String Indexing
+
+ex_str = "Python Programming"
+print(ex_str[8])
+print(ex_str[-3])
+print(ex_str[-1])
 
 # String Slicing
 
+print(ex_str[3:])
+print(ex_str[:6])
+print(ex_str[3:-4])
+print(ex_str[::])
+print(ex_str[::3])
+print(ex_str[1:10:2])
+print(ex_str[::-2])
+print(ex_str[-1:-9:-2])
+print(ex_str[::-1])
 
+# ----- @TODO Console Output -----
 
+# r
+# i
+# g
+# hon Programming
+# Python
+# hon Program
+# Python Programming
+# Ph oai
+# yhnPo
+# gimroPnhy
+# gimr
+# gnimmargorP nohtyP
+
+# ----- @TODO Console Output -----
 
