c88327a20279bfbc4c994c3e27bd6ed63ce99dc1
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2024-01-27 20:25:53 +0530

    Update string_properties.py

--- objects-and-data-structures/string_properties.py
+++ objects-and-data-structures/string_properties.py
@@ -1,1 +1,21 @@
 
+# @TODO || Immutability ||
+
+name = "Souparna"
+# name[0] = "A"
+print(name)
+
+# TypeError: 'str' object does not support item assignment
+
+# @TODO || Concatenation ||
+
+print("A" + name[3:])
+print(name[0]*10)
+
+# ----- @TODO Console Output -----
+
+# Souparna
+# Aparna
+# SSSSSSSSSS
+
+# ----- @TODO Console Output -----
