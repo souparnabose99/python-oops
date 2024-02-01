f0b05b4ed7e00e898790bf88776367eed99deb32
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2024-02-01 23:55:42 +0530

    Update string_formatting.py

--- objects-and-data-structures/string_formatting.py
+++ objects-and-data-structures/string_formatting.py
@@ -3,6 +3,10 @@
 # .format Method:
 
 print("This is regular {}".format("string"))
+print("This {} a {} {}".format("is", "regular", "string"))
+print("This {0} a {1} {2}".format("is", "regular", "string"))
+print("This {a} a {b} {c}".format(a="is", b="regular", c="string"))
+print("This {a} a {a} {a}".format(a="is", b="regular", c="string"))
 
 
 # ----- @TODO Console Output -----
