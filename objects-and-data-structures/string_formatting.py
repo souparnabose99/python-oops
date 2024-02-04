ee564afa7c81d1a60f731ffede725aa4c7d48e41
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2024-02-04 22:08:42 +0530

    Update string_formatting.py

--- objects-and-data-structures/string_formatting.py
+++ objects-and-data-structures/string_formatting.py
@@ -1,9 +1,20 @@
 ##### @TODO: String Formatting #####
 
-# % Method:
+# % Method -> % is referred to as a "string formatting operator":
 
 print("This is a %s string." %"something")
 print("This is a %s string for %s formatting." %("sample", "string"))
+
+# Format conversion methods -> 2 methods %s and %r convert any python object to a string using two separate methods: str() and repr()
+# %r and repr() deliver the string representation of the object, including quotation marks and any escape characters.
+
+print("His name is  %s" %"ABCD")
+print("His name is %r" %"ABC")
+print("This is  %s" %"AB \tCD")
+print("This is %r" %"AB \tCD")
+
+print("This no is %s" %3.75)
+print("This no is %d" %3.75)
 
 # .format Method:
 
@@ -18,6 +29,15 @@ print("This {a} a {a} {a}".format(a="is", b="regular", c="string"))
 
 # This is a something string.
 # This is a sample string for string formatting.
+
+# His name is  ABCD
+# His name is 'ABC'
+# This is  AB 	CD
+# This is 'AB \tCD'
+
+# This no is 3.75
+# This no is 3
+
 # This is regular string
 # This is a regular string
 # This is a regular string
