55e8c8718969e01071eba2e4e410aefbd8f75b1f
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2024-02-10 22:28:33 +0530

    Update files.py

--- objects-and-data-structures/files.py
+++ objects-and-data-structures/files.py
@@ -1,1 +1,3 @@
 
+# Python uses file objects to interact with external files on computer -> like audio, text, emails/excel docs
+# Need to install certain libraries & modules, to interact with those various file types
