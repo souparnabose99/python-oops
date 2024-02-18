d392d10bd67d854b65f78a188494d37cdf0b7c25
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2024-02-18 16:36:02 +0530

    Update useful-operators.py

--- objects-and-data-structures/useful-operators.py
+++ objects-and-data-structures/useful-operators.py
@@ -27,6 +27,19 @@ print(list_5)
 print(randint(0, 100))
 print(randint(0, 100))
 
+# List comprehension
+
+list_comp = [letter for letter in str_1]
+print(list_comp)
+list_comp_2 = [num**2 for num in range(1, 11)]
+print(list_comp_2)
+list_comp_3 = [num**2 for num in range(1, 11) if num % 2 == 0]
+print(list_comp_3)
+
+celcius = [0, 10, 20, 30]
+fahrenheit = [((9/5)*temp + 32) for temp in celcius]
+print(fahrenheit)
+
 # ----- @TODO Console Output -----
 
 # range(0, 10, 2)
@@ -44,3 +57,7 @@ print(randint(0, 100))
 # [5, 4, 6, 3, 2, 8, 7, 1]
 # 21
 # 24
+# ['a', 'b', 'c', 'd', 'e', 'f']
+# [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+# [4, 16, 36, 64, 100]
+# [32.0, 50.0, 68.0, 86.0]
