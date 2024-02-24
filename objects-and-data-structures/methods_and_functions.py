40e3ae0f3f5acf2f0c756b6b57f73c41f72b91a9
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2024-02-24 15:39:53 +0530

    Update methods_and_functions.py

--- objects-and-data-structures/methods_and_functions.py
+++ objects-and-data-structures/methods_and_functions.py
@@ -1,8 +1,30 @@
 
 # Methods -> Built-in objects in python
 
+list_1 = [1, 2]
+print(help(list_1.insert))
+
+
+def func_1(a, b):
+    print(a + b)
+    return a + b
+
+
+func_1(1, 2)
+print(func_1)
+
+# Python documentation: https://docs.python.org
+# Functions -> clean repeatable blocks of code that can be executed many times
+# Return allows to save the data to a variable
+
 
 # ----- @TODO Console Output -----
-
-
+# Help on built-in function insert:
+#
+# insert(index, object, /) method of builtins.list instance
+#     Insert object before index.
+#
+# None
+# 3
+# <function func_1 at 0x0000021B88693F40>
 
