687463bf3989da8f591dc93a1938e9a6822e16e2
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2024-02-26 23:56:11 +0530

    Update args_and_kwargs.py

--- objects-and-data-structures/args_and_kwargs.py
+++ objects-and-data-structures/args_and_kwargs.py
@@ -7,13 +7,20 @@ def calculate(a, b, c, d=0):
     return sum((a, b, c, d)) * 10
 
 
-def calculate_2(*args):
+def calculate_2(*args): # * sign is the main thing, we can use any other word other than args as well, eg->abcd
     return sum(args) * 10
+
+
+def calculate_3(*abc): # * sign is the main thing, we can use any other word other than args as well, eg->abcd
+    return sum(abc) * 10
 
 
 # calculate(1, 2, 3, 4, 5)
 val = calculate_2(1, 2, 3, 4, 5)
 print(val)
+
+val_2 = calculate_3(1, 2, 3, 4, 9)
+print(val_2)
 
 
 # ----- @TODO Console Output -----
@@ -22,9 +29,5 @@ print(val)
 #     calculate(1, 2, 3, 4, 5)
 # TypeError: calculate() takes from 3 to 4 positional arguments but 5 were given
 # 150
-
-
-
-
-
+# 190
 
