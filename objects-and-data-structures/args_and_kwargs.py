9b720d13611ffc1a65ae4e4080cb3da81f8e37fd
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2024-02-26 23:53:30 +0530

    Update args_and_kwargs.py

--- objects-and-data-structures/args_and_kwargs.py
+++ objects-and-data-structures/args_and_kwargs.py
@@ -1,3 +1,30 @@
 
+# args -> arguments, kwargs -> keyword arguments
+
+# args is like a tuple
+
+def calculate(a, b, c, d=0):
+    return sum((a, b, c, d)) * 10
+
+
+def calculate_2(*args):
+    return sum(args) * 10
+
+
+# calculate(1, 2, 3, 4, 5)
+val = calculate_2(1, 2, 3, 4, 5)
+print(val)
+
+
+# ----- @TODO Console Output -----
+# Traceback (most recent call last):
+#   File "F:\Data-Structures-Algorithms\Data-Structures\Objects-And-Data-Structures\args_and_kwargs.py", line 10, in <module>
+#     calculate(1, 2, 3, 4, 5)
+# TypeError: calculate() takes from 3 to 4 positional arguments but 5 were given
+# 150
+
+
+
+
 
 
