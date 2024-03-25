bbbf1d3cf8243300c92fb1afd7bc65511831b6ba
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2024-03-25 22:37:11 +0530

    Update class_and_attributes.py

--- object-oriented-programming/class_and_attributes.py
+++ object-oriented-programming/class_and_attributes.py
@@ -1,6 +1,20 @@
 
 
+class ProgrammingLanguages:
+
+    def __init__(self, language):
+        self.language = language
 
 
+# pl = ProgrammingLanguages()
+# TypeError: ProgrammingLanguages.__init__() missing 1 required positional argument: 'language'
+pl = ProgrammingLanguages("Java")
+print(type(pl))
+print(pl.language)
+
+# ----- @TODO Console Output -----
+
+# <class '__main__.ProgrammingLanguages'>
+# Java
 
 
