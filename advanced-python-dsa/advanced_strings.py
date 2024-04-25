0d5277c9c7ea4faab2c36c47cfd609efd0c85df0
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2024-04-25 09:16:30 +0530

    Update advanced_strings.py

--- advanced-python-dsa/advanced_strings.py
+++ advanced-python-dsa/advanced_strings.py
@@ -1,1 +1,17 @@
 
+str_1 = "Python programming"
+print(str_1.capitalize())
+print(str_1.upper())
+print(str_1.lower())
+print(str_1.lower().count("p"))
+print(str_1.lower().find("p"))
+
+
+# ----- @TODO Console Output -----
+
+# Python programming
+# PYTHON PROGRAMMING
+# python programming
+# 2
+# 0
+
