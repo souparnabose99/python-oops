015ca11f7d6fd5423e7e6073754da7a88ca360e7
Souparna Bose <souparnabose99@gmail.com>
2024-05-01 11:21:09 +0530

    Update project files

--- advanced-python-dsa/advanced_strings.py
+++ advanced-python-dsa/advanced_strings.py
@@ -1,10 +1,14 @@
 
 str_1 = "Python programming"
+str_2 = "Hello"
 print(str_1.capitalize())
 print(str_1.upper())
 print(str_1.lower())
 print(str_1.lower().count("p"))
 print(str_1.lower().find("p"))
+print(str_2.isalnum())
+print("hihihihihihhhh".split("i"))
+print("hihihihihihhhh".partition("i"))
 
 
 # ----- @TODO Console Output -----
@@ -14,4 +18,7 @@ print(str_1.lower().find("p"))
 # python programming
 # 2
 # 0
+# True
+# ['h', 'h', 'h', 'h', 'h', 'hhhh']
+# ('h', 'i', 'hihihihihhhh')
 
