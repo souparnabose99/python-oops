dc78a650ab838adcaa51defae7e6268791e9c148
Souparna Bose <souparnabose99@gmail.com>
2024-05-02 11:32:55 +0530

    Update project files

---
+++ advanced-python-dsa/advanced_sets.py
@@ -0,0 +1,26 @@
+
+s = set()
+s_2 = set()
+s_2.add(1)
+
+s.add(1)
+s.add(2)
+s.add(3)
+print(s)
+
+s.add(3)
+print(s)
+
+print(s.intersection(s_2))
+print(s.symmetric_difference(s_2))
+print(s.isdisjoint(s_2))
+print(s.union(s_2))
+print(s.difference(s_2))
+
+s.discard(2)
+print(s)
+
+# ----- @TODO Console Output -----
+
+
+
