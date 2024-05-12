d53422cb150cb7968bdbb4ce8a49ad2d35dcaed3
Souparna Bose <souparnabose99@gmail.com>
2024-05-13 02:07:36 +0530

    Update project files

--- advanced-python-modules/regex.py
+++ advanced-python-modules/regex.py
@@ -1,1 +1,17 @@
+import re
 
+sample_text = "The associate phone number is 111-222-3333"
+match = re.search("phone", sample_text)
+
+print(match)  # -> finds & returns only the first match
+print(match.span())
+print(match.start())
+print(match.end())
+
+
+# ----- @TODO Console Output -----
+
+# <re.Match object; span=(14, 19), match='phone'>
+# (14, 19)
+# 14
+# 19
