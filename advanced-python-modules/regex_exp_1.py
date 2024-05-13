9da42f4e8ffc38c6e6021858fcb4ca57230469d5
Souparna Bose <souparnabose99@gmail.com>
2024-05-14 02:41:48 +0530

    Update project files

--- advanced-python-modules/regex_exp_1.py
+++ advanced-python-modules/regex_exp_1.py
@@ -14,6 +14,21 @@ print(new_matches.group(1))
 print(new_matches.group(2))
 print(new_matches.group(3))
 
+# Additional Regex
+
+# OR operator
+print(re.search(r"sat|hat", "The man sat").group())
+print(re.search(r"sat|hat", "The man had a hat").group())
+
+# Wildcard search
+print(re.findall(r".at", "The man sat with the hat and then spat"))
+print(re.findall(r"..at", "The man sat with the hat and then spat"))
+print(re.findall(r"...at", "The man sat with the hat and then spat"))
+
+print(re.findall(r"^\d", "4 is an even number"))
+print(re.findall(r"\d$", "Output is 0"))
+print(type(re.findall(r"\d$", "Output is 0")[0]))
+
 # ----- @TODO Console Output -----
 
 # 111-222-3333
@@ -22,3 +37,11 @@ print(new_matches.group(3))
 # 111
 # 222
 # 3333
+# sat
+# hat
+# ['sat', 'hat', 'pat']
+# [' sat', ' hat', 'spat']
+# ['n sat', 'e hat', ' spat']
+# ['4']
+# ['0']
+# <class 'str'>
