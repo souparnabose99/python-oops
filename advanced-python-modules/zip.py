132bdd589a24918ab06c9713c4d08893205a9a53
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2024-05-15 21:01:27 +0530

    Update zip.py

--- advanced-python-modules/zip.py
+++ advanced-python-modules/zip.py
@@ -1,6 +1,10 @@
+import shutil
 
+folder_to_zip = r"C:\Users\HP\Downloads\sample-zip"
+print(shutil.make_archive(r"C:\Users\HP\Downloads\zipped", "zip", folder_to_zip))
 
+shutil.make_archive(r"C:\Users\HP\Downloads\zipped.zip", "unzipped", "zip")
 
 # ----- @TODO Console Output -----
 
-
+# C:\Users\HP\Downloads\zipped.zip
